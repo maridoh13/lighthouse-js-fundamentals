864854d390de2add266a66487ba85a394b411e8a
Mariana Dominguez <59099498+maridoh13@users.noreply.github.com>
2020-01-12 22:15:25 -0800

    Update loopy.js

--- loopy.js
+++ loopy.js
@@ -3,17 +3,18 @@
 // multiple of 3 and 4 - LoopyLighthouse
 // from 100 to 200
 
-for (var x = 100; x <=200; x++) {
-    if ((x % 3 === 0) && (x % 4 === 0)) {
-        console.log("LoopyLighthouse");
-    }
-    else if (x % 3 === 0) {
-        console.log("Loopy");
-    }
-    else if (x % 4 === 0) {
-        console.log("Lighthouse");
-    }
-    else {
-        console.log(x)
-    }
+for (var x = 100; x <= 200; x++) {
+  if ((x % 3 === 0) && (x % 4 === 0)) {
+    console.log("LoopyLighthouse");
+  }
+  else if (x % 3 === 0) {
+    console.log("Loopy");
+  }
+  else if (x % 4 === 0) {
+    console.log("Lighthouse");
+  }
+  
+  else {
+    console.log(x);
+  }
 }
